6406148af192bd01d1107c0e520f0e50597f6680
Anthony Zhou <anthony.zhou03@gmail.com>
2020-05-13 14:40:07 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import logo from './logo.svg';
+import './App.css';
+import Dashboard from './pages/Dashboard';
+import Home from './pages/Home';
+import Otherpage from './pages/Otherpage';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+            <li>
+              <Link to="/otherpage">Otherpage</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/otherpage">
+            <Otherpage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/Otherpage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Otherpage() {
+    return (<div>
+        Welcome to the other page! (Not sure why this one is here)
+    </div>)
+}
+
+export default Otherpage;
