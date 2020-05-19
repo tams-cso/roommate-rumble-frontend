e9c7be1ad76aec3b402faa4feb760ab0130ee240
Jeongwoo Choi <iamchoij@gmail.com>
2020-05-19 00:51:50 -0500

    Updates

---
+++ precache-manifest.c85a7db16a0b662ea4a22a93f2128d54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96cd0fa6dc6f728620ff402dc5b44ef3",
+    "url": "/roommate-rumble-frontend/index.html"
+  },
+  {
+    "revision": "dc1a3877dd9ce5aa4fdc",
+    "url": "/roommate-rumble-frontend/static/css/main.b98de666.chunk.css"
+  },
+  {
+    "revision": "53caf60f251e11ac23a4",
+    "url": "/roommate-rumble-frontend/static/js/2.d5402c1b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/roommate-rumble-frontend/static/js/2.d5402c1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc1a3877dd9ce5aa4fdc",
+    "url": "/roommate-rumble-frontend/static/js/main.a893e5c3.chunk.js"
+  },
+  {
+    "revision": "92f71921ece2ad5ddb1e",
+    "url": "/roommate-rumble-frontend/static/js/runtime-main.71ced8ad.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/roommate-rumble-frontend/static/media/logo.ee7cd8ed.svg"
+  }
+]);
